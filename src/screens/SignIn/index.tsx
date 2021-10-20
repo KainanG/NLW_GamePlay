1deae9930849d74b45e5f68f9e95a1d4b4c252c2
KainanG <kainangomes2016@gmail.com>
2021-10-20 00:20:39 -0300

    Adicionando a tela de Entrada

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,46 @@
-import React from "react";
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, Text, TextInput, Image, StatusBar, } from 'react-native'
+
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from './styles';
 
 export function SignIn() {
+    const [text, setText] = useState('');
+
     return (
-        <View>
-            <Text>Hello World, NLW Together</Text>
+        <View style={styles.container}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+
+            <Image
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Conecte-se {`\n`}
+                    e organize suas {`\n`}
+                    jogatinas
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon
+                    title="Entrar com Discord"
+                />
+
+
+            </View>
+
         </View>
     );
 }
